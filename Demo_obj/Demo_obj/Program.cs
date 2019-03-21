a2bc1e35906dc4f27e862e84ae2e2b2e6fd21621
joe-home <251179282@qq.com>
2019-03-21 13:38:39 +0800

    引用类型

---
+++ Demo_obj/Demo_obj/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Demo_obj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 5; //值类型
+            object obj;//引用类型
+            obj = i;//装箱
+            int x = (int)obj;//拆箱
+
+            System.Console.WriteLine("x={0}", x);
+            System.Console.ReadKey();
+        }
+    }
+}
